9c7b3080b0b5ecc16d422473ceff80bab5943dcc
Gustavo DeLlac <gustavodellac@gmail.com>
2022-10-16 19:19:51 -0300

    iniciamos proyecto celulares en java

---
+++ ProyectoCelularesOmega/src/celularesomega/Celular.java
@@ -0,0 +1,22 @@
+
+package celularesomega;
+
+public class Celular {
+    String marca;
+    String modelo;
+    String caracteristicas;
+    double precio;
+    
+    public Celular(String marca, String modelo, String caracteristicas, 
+            double precio){
+        
+        this.marca = marca;
+        this.modelo = modelo;
+        this.caracteristicas = caracteristicas;
+        this.precio = precio;
+    }
+    
+    public String verCaracteristicas(){
+        return this.caracteristicas;
+    }
+}
