5ec81c5a75aeae96f7fe26216a028e767eaa5570
Gustavo DeLlac <gustavodellac@gmail.com>
2022-11-24 12:42:02 -0300

    agrego comentarios al código

--- ProyectoCelularesOmega/src/main/MetodoDePago.java
+++ ProyectoCelularesOmega/src/main/MetodoDePago.java
@@ -2,24 +2,28 @@
 package main;
 
 
-public class MetodoDePago {
-
+public abstract class MetodoDePago {
+    
+    //método estático para calcular descuento 15 %
     public static double calcularDescuentoQuincePorciento(double precio){
         double precioResultado = precio - precio* 15/100;
         return precioResultado;
     }
     
+    //método estático para calcular descuento 10 %
     public static double calcularDescuentoDiezPorciento(double precio){
         double precioResultado = precio - precio* 10/100;
         return precioResultado;
     }
     
+    //método estático para calcular 6 cuotas
     public static double calcularCuotasSeis(double precio){
         double precioResultado = precio / 6;
         return precioResultado;
     }
     
-     public static double calcularCuotasDoce(double precio){
+    //método estático para calcular 12 cuotas
+    public static double calcularCuotasDoce(double precio){
         double precioResultado = precio / 12;
         return precioResultado;
     }
